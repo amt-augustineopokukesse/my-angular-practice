8b0211c7fc714ca7171e9006ebc4c839f8ac03b5
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-08 15:40:11 +0000

    feat: formbuilder with formarray

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-editor.component.html',
   styleUrl: './profile-editor.component.css'
 })
@@ -18,8 +19,25 @@ export class ProfileEditorComponent {
       state: [''],
       zip: [''],
     }),
-
-  })
-
+    aliases: this.formBuilder.array([this.formBuilder.control('')]),
+  });
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
   constructor(private formBuilder: FormBuilder) {}
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+  addAlias() {
+    this.aliases.push(this.formBuilder.control(''));
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
